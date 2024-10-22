1d0bcc7fcf89dd3496b7bb5e24ef029a0b2b04ea
Dhea.Rizky <dhea.rizky@formulatrix.com>
2024-10-22 23:11:13 +0700

    pygame

--- Python/01.Pygame/menggambar bentuk.py
+++ Python/01.Pygame/menggambar bentuk.py
@@ -1,3 +1,43 @@
 import pygame
 
-#inisialisasi 
+#inisialisasi pygame
+pygame.init()
+
+#ukuran window
+screen_width = 800
+screen_height = 600
+
+#membuat window
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+#warna
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+
+#game loop
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #menggambar garis
+    pygame.draw.line(screen,red,(0,0), (800,600),5)
+
+    #menggambar lingkaran
+    pygame.draw.circle(screen,green,(400,300),50,0)
+
+    #menggambar segitiga
+    pygame.draw.polygon(screen,blue,[(100,100),(200,50),(300,100)],0)
+
+    #menggambar elips
+    pygame.draw.ellipse(screen, blue, (400, 200, 150,100),0)
+
+    #update window
+    pygame.display.update()
+
+#keluar dari Pygame
+pygame.quit()
